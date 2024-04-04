9e8dd008796581fa9ffb49b641d788973d142a52
huycuongngo <cuong.nh@softenmind.com>
2024-04-04 20:05:30 +0700

    update

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+
+const AppContext = React.createContext()
+
+// get data from store to component
+const useGlobalContext = () => {
+  return useContext(AppContext)
+}
 
 function AppProvider() {
+  const [meals, setMeals] = useState([])
+  
   return (
     <div>AppProvider</div>
   )
